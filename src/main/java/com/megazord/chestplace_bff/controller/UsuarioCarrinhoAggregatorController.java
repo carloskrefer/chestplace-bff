96a81b83e23cbf520db886285d2bf7940b0c9503
Carlos Krefer <carloskrefer@hotmail.com>
2024-11-10 13:12:37 -0300

    Adiciona agregador

---
+++ src/main/java/com/megazord/chestplace_bff/controller/UsuarioCarrinhoAggregatorController.java
@@ -0,0 +1,54 @@
+package com.megazord.chestplace_bff.controller;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+import com.megazord.chestplace_bff.helpers.ResponseEntityHelper;
+
+@RestController
+@RequestMapping("/api/aggregate/usuario-carrinho")
+public class UsuarioCarrinhoAggregatorController {
+    private String CARRINHO_URL = "https://chestplace-carrinho.azurewebsites.net/api/carrinhos/";
+    private String USUARIO_URL = "https://chestplace-egceafa0bge8d9bz.brazilsouth-01.azurewebsites.net/users/";
+    private RestTemplate restTemplate;
+
+    public UsuarioCarrinhoAggregatorController(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @GetMapping("/{compradorId}")
+    public ResponseEntity<String> getCarrinhoEUsuario(@PathVariable int compradorId) {
+        try {
+            ResponseEntity<String> carrinhoResponse = restTemplate.getForEntity(
+                CARRINHO_URL + compradorId, 
+                String.class
+            );
+
+            ResponseEntity<String> usuarioResponse = restTemplate.getForEntity(
+                USUARIO_URL + compradorId, 
+                String.class
+            );
+
+            String jsonResponse = String.format(
+                "{\"carrinho\": %s, \"usuario\": %s}", 
+                carrinhoResponse.getBody(), 
+                usuarioResponse.getBody()
+            );
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(jsonResponse);
+
+        } catch (RestClientResponseException e) {
+            return ResponseEntityHelper.getResponseEntityByException(e);
+        }
+    }
+
+}
