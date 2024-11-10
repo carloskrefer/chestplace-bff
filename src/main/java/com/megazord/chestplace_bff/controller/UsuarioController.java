8f7bdb41d9bbf983a0c049fc59a34fce6845c49e
Carlos Krefer <carloskrefer@hotmail.com>
2024-11-10 12:16:13 -0300

    Adiciona controller de usuário

---
+++ src/main/java/com/megazord/chestplace_bff/controller/UsuarioController.java
@@ -0,0 +1,102 @@
+package com.megazord.chestplace_bff.controller;
+
+import java.util.Map;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+import com.megazord.chestplace_bff.helpers.ResponseEntityHelper;
+
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+    private String URL = "https://chestplace-egceafa0bge8d9bz.brazilsouth-01.azurewebsites.net/users";
+    private RestTemplate restTemplate;
+
+    public UsuarioController(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<String> getAllUsers() {
+        try {
+            return restTemplate.getForEntity(
+                URL, 
+                String.class
+            );
+        } catch(RestClientResponseException e) {
+            return ResponseEntityHelper.getResponseEntityByException(e);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getCarrinho(@PathVariable int id) {
+        try {
+            return restTemplate.getForEntity(
+                URL + "/" + id, 
+                String.class
+            );
+        } catch(RestClientResponseException e) {
+            return ResponseEntityHelper.getResponseEntityByException(e);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> postUser(
+        @RequestBody Map<String, Object> user
+    ) {
+        try {
+            return restTemplate.postForEntity(
+                URL + "/register", 
+                user, 
+                String.class
+            );
+        } catch(RestClientResponseException e) {
+            return ResponseEntityHelper.getResponseEntityByException(e);
+        }
+    } 
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> putCarrinho(
+        @PathVariable int id,
+        @RequestBody Map<String, Object> user
+    ) {
+        try {
+            return restTemplate.exchange(
+                URL + "/" + id, 
+                HttpMethod.PUT, 
+                new HttpEntity<>(user), 
+                String.class
+            );
+        } catch(RestClientResponseException e) {
+            return ResponseEntityHelper.getResponseEntityByException(e);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCarrinho(@PathVariable int id) {
+        try {
+            return restTemplate.exchange(
+                URL + "/" + id,
+                HttpMethod.DELETE, 
+                null, 
+                String.class
+            );
+        } catch(RestClientResponseException e) {
+            return ResponseEntityHelper.getResponseEntityByException(e);
+        }
+    }
+
+}
